c5c802b9ca5307b1863eee71002e8529fce7f61b
Abhishek <abhishek.amishra@gmail.com>
2020-07-12 23:23:02 +0530

    RV layout

---
+++ app/src/main/java/com/kjfmbktgl4/fintrack/ui/TransactionRV.java
@@ -0,0 +1,16 @@
+package com.kjfmbktgl4.fintrack.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.kjfmbktgl4.fintrack.R;
+
+public class TransactionRV extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
