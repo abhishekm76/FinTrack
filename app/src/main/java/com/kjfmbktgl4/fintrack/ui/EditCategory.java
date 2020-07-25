a0af1314698c811cf4ae8508beaa6a58a0a5ad35
Abhishek <abhishek.amishra@gmail.com>
2020-07-25 22:01:02 +0530

    Reverted seems to be working

---
+++ app/src/main/java/com/kjfmbktgl4/fintrack/ui/EditCategory.java
@@ -0,0 +1,16 @@
+package com.kjfmbktgl4.fintrack.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kjfmbktgl4.fintrack.R;
+
+public class EditCategory extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_category);
+	}
+}
