f4cc0c1997b70820cbf34b3867d592b31fdca09d
abhishekm76 <abhishek.amishra@gmail.com>
2020-08-04 00:13:21 +0530

    Added date selection except for custom dates

--- app/src/main/java/com/kjfmbktgl4/fintrack/ui/PieChart.java
+++ app/src/main/java/com/kjfmbktgl4/fintrack/ui/PieChart.java
@@ -5,28 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.material.textfield.TextInputEditText;
 import com.kjfmbktgl4.fintrack.R;
 import com.kjfmbktgl4.fintrack.data.DatabaseHandler;
 import com.kjfmbktgl4.fintrack.model.CategoryTotals;
 import com.kjfmbktgl4.fintrack.util.DateConverters;
 import com.kjfmbktgl4.fintrack.util.Util;
 
+import java.text.DateFormat;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class PieChart extends AppCompatActivity {
+public class PieChart extends AppCompatActivity implements View.OnClickListener{
 	private ArrayList<CategoryTotals> dataArrayList;
 	private ArrayList<CategoryTotals> transactionItemArrayListByCategory = new ArrayList<>();
 	private String startDateString,endDateString;
+	private TextInputEditText startDateET, endDateET;
+	private Button allButton,yearButton,monthButton,refreshButton,todayButton;
 	com.github.mikephil.charting.charts.PieChart pieChart;
 
 	@Override
@@ -34,22 +41,40 @@ public class PieChart extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pie_chart);
 		pieChart = findViewById(R.id.piechart);
+		startDateET=findViewById(R.id.selectStartDateForPie);
+		endDateET=findViewById(R.id.selectEndDateForPie);
+		allButton = findViewById(R.id.btn_all);
+		yearButton=findViewById(R.id.btn_2018);
+		monthButton=findViewById(R.id.btn_thisMonth);
+		todayButton=findViewById(R.id.btn_today);
+		refreshButton=findViewById(R.id.refreshGraphs);
+
+		allButton.setOnClickListener(this);
+		yearButton.setOnClickListener(this);
+		monthButton.setOnClickListener(this);
+		todayButton.setOnClickListener(this);
+		refreshButton.setOnClickListener(this);
+
+
+
 		setStartAndEndDates();
+		setDatePickers();
 		dataArrayList= getDataForGraph();
 		drawPiechart(dataArrayList);
+	}
+
+	private void setDatePickers() {
+		String startDate = DateConverters.longStringToDateString(startDateString);
+		startDateET.setText(startDate);
+		String endDate = DateConverters.longStringToDateString(endDateString);
+		endDateET.setText(endDate);
 	}
 
 	private void setStartAndEndDates() {
 		if (startDateString==null || endDateString==null){
 			Date endDate=Calendar.getInstance().getTime();
-			//endDateString = String.valueOf(DateConverters.dateToLong(endDate));
 			endDateString = DateConverters.getcurrentDateInMilLs();
 			startDateString=DateConverters.getFirstOfCurrentYearInMills();
-
-			/*Calendar cal = Calendar.getInstance();
-			cal.set(Calendar.DAY_OF_YEAR, 1);
-			Date startDate = cal.getTime();
-			startDateString= String.valueOf(DateConverters.dateToLong(startDate));*/
 			Log.d(Util.TAG,startDateString+" "+endDateString);
 
 		}
@@ -85,4 +110,66 @@ public class PieChart extends AppCompatActivity {
 		pieChart.animateY(1500);
 		pieChart.invalidate();
 	}
+
+	public void drawAllData(){
+	// set start and end dates to cover all data
+		startDateString = DateConverters.getEpochStart();
+		endDateString = DateConverters.getcurrentDateInMilLs();
+		Log.d(Util.TAG,"all "+startDateString+" "+endDateString);
+
+	}
+
+	public void drawThisYear(){
+		// set start and end dates to cover this year
+		startDateString = DateConverters.getFirstOfCurrentYearInMills();
+		endDateString = DateConverters.getcurrentDateInMilLs();
+		Log.d(Util.TAG,"all "+startDateString+" "+endDateString);
+
+	}
+	public void drawThisMonth(){
+		startDateString = DateConverters.getFirstOfMonth();
+		endDateString = DateConverters.getcurrentDateInMilLs();
+		Log.d(Util.TAG,"month "+startDateString+" "+endDateString);
+
+		// set start and end dates to cover this month
+	}
+	public void drawToday(){
+		startDateString = DateConverters.getFirstOfWeek();
+		endDateString = DateConverters.getcurrentDateInMilLs();
+		Log.d(Util.TAG,"month "+startDateString+" "+endDateString);
+
+
+
+
+		// set start and end dates to cover today
+	}
+	public void drawCustom(){
+		// set start and end dates to cover custom period
+	}
+
+	@Override
+	public void onClick(View pView) {
+		switch (pView.getId()) {
+			case R.id.btn_all:
+ 				drawAllData();
+				break;
+			case R.id.btn_2018:
+				drawThisYear();
+				break;
+			case R.id.btn_thisMonth:
+				drawThisMonth();
+				break;
+			case R.id.btn_today:
+				drawToday();
+				break;
+			case R.id.refreshGraphs:
+				drawCustom();
+				break;
+
+ 		}
+		setDatePickers();
+		dataArrayList= getDataForGraph();
+		drawPiechart(dataArrayList);
+	}
+
 }
--- app/src/main/java/com/kjfmbktgl4/fintrack/util/DateConverters.java
+++ app/src/main/java/com/kjfmbktgl4/fintrack/util/DateConverters.java
@@ -2,6 +2,7 @@ package com.kjfmbktgl4.fintrack.util;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.time.OffsetDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,13 +18,19 @@ public class DateConverters {
 		Long dateinmillis = date.getTime();
 		return dateinmillis;
 	}
+
+	public static String longStringToDateString(String dateInMillis){
+		Long dateInLong= Long.parseLong(dateInMillis);
+		String stringDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(dateInLong);
+		return stringDate;
+	}
+
 	public static String getcurrentDateInMilLs(){
 		Calendar cal1 = Calendar.getInstance();
-		cal1.add(Calendar.DAY_OF_YEAR, 1);
-		cal1.set(Calendar.SECOND, 0);
-		cal1.set(Calendar.MINUTE, 0);
-		cal1.set(Calendar.HOUR_OF_DAY, 0);
-		cal1.set(Calendar.MILLISECOND,0);
+		cal1.set(Calendar.SECOND, 59);
+		cal1.set(Calendar.MINUTE, 59);
+		cal1.set(Calendar.HOUR_OF_DAY, 23);
+		cal1.set(Calendar.MILLISECOND,999);
 		Date endDate= cal1.getTime();
 		String curDateString = String.valueOf(DateConverters.dateToLong(endDate));
 		return curDateString;
@@ -31,6 +38,39 @@ public class DateConverters {
 	public static String getFirstOfCurrentYearInMills(){
 		Calendar cal = Calendar.getInstance();
 		cal.set(Calendar.DAY_OF_YEAR, 1);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MILLISECOND,0);
+		Date firstDate = cal.getTime();
+		String firstDateString= String.valueOf(DateConverters.dateToLong(firstDate));
+		return firstDateString;
+	}
+	public static String getFirstOfMonth(){
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MILLISECOND,0);
+		Date firstDate = cal.getTime();
+		String firstDateString= String.valueOf(DateConverters.dateToLong(firstDate));
+		return firstDateString;
+	}
+	public static String getEpochStart(){
+		Calendar cal = Calendar.getInstance();
+		cal.set(2000,0,1);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MILLISECOND,0);
+		Date firstDate = cal.getTime();
+		String firstDateString= String.valueOf(DateConverters.dateToLong(firstDate));
+		return firstDateString;
+	}public static String getFirstOfWeek(){
+		Calendar cal = Calendar.getInstance();
+
+		cal.set(Calendar.DAY_OF_WEEK,1);
 		cal.set(Calendar.SECOND, 0);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.HOUR_OF_DAY, 0);
