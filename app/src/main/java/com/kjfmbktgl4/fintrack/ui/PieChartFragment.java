298200c88b3b64e976cdf9747dc9bff82fe3dcc3
Abhishek <abhishek.amishra@gmail.com>
2020-08-14 18:04:17 +0530

    PieChart converted to fragment_pie_chart.xml

--- app/src/main/java/com/kjfmbktgl4/fintrack/ui/PieChartFragment.java
+++ app/src/main/java/com/kjfmbktgl4/fintrack/ui/PieChartFragment.java
@@ -1,26 +1,27 @@
 package com.kjfmbktgl4.fintrack.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.NavUtils;
-
 import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Switch;
 
-import com.github.mikephil.charting.components.Description;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.kjfmbktgl4.fintrack.R;
@@ -30,14 +31,12 @@ import com.kjfmbktgl4.fintrack.util.DateConverters;
 import com.kjfmbktgl4.fintrack.util.Util;
 
 import java.text.DateFormat;
-import java.time.Year;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
-public class PieChart extends AppCompatActivity /*implements View.OnClickListener */{
-	/*private ArrayList<CategoryTotals> dataArrayList;
+public class PieChartFragment extends Fragment implements View.OnClickListener {
+	private ArrayList<CategoryTotals> dataArrayList;
 	private ArrayList<CategoryTotals> transactionItemArrayListByCategory = new ArrayList<>();
 	private String startDateString, endDateString;
 	private TextInputEditText startDateET, endDateET;
@@ -45,32 +44,27 @@ public class PieChart extends AppCompatActivity /*implements View.OnClickListene
 	private Boolean usePercent = true;
 	private Switch switchButton;
 	private Button allButton, yearButton, monthButton, refreshButton, todayButton;
-	com.github.mikephil.charting.charts.PieChart pieChart;*/
+	com.github.mikephil.charting.charts.PieChart pieChart;
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		View v = inflater.inflate(R.layout.fragment_pie_chart,container,false);
+		pieChart = v.findViewById(R.id.piechart);
+		startDateET = v.findViewById(R.id.selectStartDateForPie);
+		endDateET = v.findViewById(R.id.selectEndDateForPie);
+		allButton = v.findViewById(R.id.btn_all);
+		yearButton = v.findViewById(R.id.btn_2018);
+		monthButton = v.findViewById(R.id.btn_thisMonth);
+		todayButton = v.findViewById(R.id.btn_today);
+		refreshButton = v.findViewById(R.id.refreshGraphs);
+		switchButton = v.findViewById(R.id.switch_percent);
+		return v;
+	}
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_pie_chart);
-
-		if (savedInstanceState == null) {
-			PieChartFragment fragment = new PieChartFragment();
-			/*fragment.setArguments(getIntent().getExtras());*/
-			getSupportFragmentManager()
-					.beginTransaction()
-					.add(R.id.container_pieChart, fragment)
-					.commit();
-		}
-
-	}
-	/*	pieChart = findViewById(R.id.piechart);
-		startDateET = findViewById(R.id.selectStartDateForPie);
-		endDateET = findViewById(R.id.selectEndDateForPie);
-		allButton = findViewById(R.id.btn_all);
-		yearButton = findViewById(R.id.btn_2018);
-		monthButton = findViewById(R.id.btn_thisMonth);
-		todayButton = findViewById(R.id.btn_today);
-		refreshButton = findViewById(R.id.refreshGraphs);
-		switchButton = findViewById(R.id.switch_percent);
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
 		allButton.setOnClickListener(this);
 		yearButton.setOnClickListener(this);
 		monthButton.setOnClickListener(this);
@@ -78,14 +72,16 @@ public class PieChart extends AppCompatActivity /*implements View.OnClickListene
 		refreshButton.setOnClickListener(this);
 		startDateET.setOnClickListener(this);
 		endDateET.setOnClickListener(this);
-
+		switchSetUp();
 
 		setUpToolbar();
 		setStartAndEndDates();
 		setDatePickers();
 		dataArrayList = getDataForGraph();
 		drawPiechart(dataArrayList);
+	}
 
+	private void switchSetUp() {
 		switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				if (isChecked) {
@@ -100,24 +96,67 @@ public class PieChart extends AppCompatActivity /*implements View.OnClickListene
 		});
 	}
 
-	private void setDatePickers() {
-		String startDate = DateConverters.longStringToDateString(startDateString);
-		startDateET.setText(startDate);
-		String endDate = DateConverters.longStringToDateString(endDateString);
-		endDateET.setText(endDate);
-	}*/
+	@Override
+	public void onClick(View pView) {
+		switch (pView.getId()) {
+			case R.id.btn_all:
+				drawAllData();
+				setDate = false;
+				break;
+			case R.id.btn_2018:
+				drawThisYear();
+				setDate = false;
+				break;
+			case R.id.btn_thisMonth:
+				drawThisMonth();
+				setDate = false;
+				break;
+			case R.id.btn_today:
+				drawToday();
+				setDate = false;
+				break;
+			case R.id.refreshGraphs:
+				drawCustom();
+				break;
 
-	public void setUpToolbar(String pSubtitle) {
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+			case R.id.selectStartDateForPie:
+			case R.id.selectEndDateForPie:
+				pickerShow(pView);
+				setDate = true;
+				break;
+		}
 
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setTitle("FinTrack");
-		getSupportActionBar().setSubtitle(pSubtitle);
+		if (!setDate) {
+			setDatePickers();
+			dataArrayList = getDataForGraph();
+			drawPiechart(dataArrayList);
+		}
+
 
 	}
 
-	/*private void setStartAndEndDates() {
+	public void pickerShow(final View pView) {
+		final Calendar cldr = Calendar.getInstance();
+		int day = cldr.get(Calendar.DAY_OF_MONTH);
+		int month = cldr.get(Calendar.MONTH);
+		int year = cldr.get(Calendar.YEAR);
+		// date picker dialog
+		DatePickerDialog picker = new DatePickerDialog(getContext(),
+				new DatePickerDialog.OnDateSetListener() {
+					@Override
+					public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+						cldr.set(year, monthOfYear, dayOfMonth);
+						String setDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(cldr.getTime());
+						EditText setDateET = (EditText) pView;
+						setDateET.setText(setDate);
+
+					}
+				}, year, month, day);
+		picker.show();
+
+
+	}
+	private void setStartAndEndDates() {
 		if (startDateString == null || endDateString == null) {
 			//Date endDate=Calendar.getInstance().getTime();
 			endDateString = DateConverters.getcurrentDateInMilLs();
@@ -129,7 +168,7 @@ public class PieChart extends AppCompatActivity /*implements View.OnClickListene
 	}
 
 	private ArrayList<CategoryTotals> getDataForGraph() {
-		DatabaseHandler db = new DatabaseHandler(PieChart.this);
+		DatabaseHandler db = new DatabaseHandler(getContext());
 		transactionItemArrayListByCategory = (ArrayList<CategoryTotals>) db.getTransactionsByCategoryByPeriod(startDateString, endDateString);
 		return transactionItemArrayListByCategory;
 	}
@@ -209,73 +248,17 @@ public class PieChart extends AppCompatActivity /*implements View.OnClickListene
 			drawPiechart(dataArrayList);
 		}
 		else{
-		Snackbar.make(startDateET, "Please select a start date that is before the end date" , Snackbar.LENGTH_LONG).show();
-		Log.d(Util.TAG, "custom " + startDateString + " " + endDateString);}
+			Snackbar.make(startDateET, "Please select a start date that is before the end date" , Snackbar.LENGTH_LONG).show();
+			Log.d(Util.TAG, "custom " + startDateString + " " + endDateString);}
 	}
-	@Override
-	public void onBackPressed(){
-		NavUtils.navigateUpFromSameTask(this);
-	}
-
-	@Override
-	public void onClick(View pView) {
-		switch (pView.getId()) {
-			case R.id.btn_all:
-				drawAllData();
-				setDate = false;
-				break;
-			case R.id.btn_2018:
-				drawThisYear();
-				setDate = false;
-				break;
-			case R.id.btn_thisMonth:
-				drawThisMonth();
-				setDate = false;
-				break;
-			case R.id.btn_today:
-				drawToday();
-				setDate = false;
-				break;
-			case R.id.refreshGraphs:
-				drawCustom();
-				break;
-
-			case R.id.selectStartDateForPie:
-			case R.id.selectEndDateForPie:
-				pickerShow(pView);
-				setDate = true;
-				break;
-		}
-
-		if (!setDate) {
-			setDatePickers();
-			dataArrayList = getDataForGraph();
-			drawPiechart(dataArrayList);
-		}
-
-
+	private void setDatePickers() {
+		String startDate = DateConverters.longStringToDateString(startDateString);
+		startDateET.setText(startDate);
+		String endDate = DateConverters.longStringToDateString(endDateString);
+		endDateET.setText(endDate);
 	}
 
-	public void pickerShow(final View pView) {
-		final Calendar cldr = Calendar.getInstance();
-		int day = cldr.get(Calendar.DAY_OF_MONTH);
-		int month = cldr.get(Calendar.MONTH);
-		int year = cldr.get(Calendar.YEAR);
-		// date picker dialog
-		DatePickerDialog picker = new DatePickerDialog(PieChart.this,
-				new DatePickerDialog.OnDateSetListener() {
-					@Override
-					public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-						cldr.set(year, monthOfYear, dayOfMonth);
-						String setDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(cldr.getTime());
-						EditText setDateET = (EditText) pView;
-						setDateET.setText(setDate);
-
-					}
-				}, year, month, day);
-		picker.show();
-
-
-	}*/
-
+	private void setUpToolbar() {
+		((PieChart)getActivity()).setUpToolbar("Distribution Graph");
+	}
 }
